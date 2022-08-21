e3aa7911c3d952730585592e9cc46058ab1a04be
Kamil <kamil.marczak@interia.pl>
2022-08-21 14:54:51 +0200

    made board generation and basic styles

---
+++ js/app.js
@@ -0,0 +1,20 @@
+// START Generate Board
+let board = document.querySelector('#board');
+let fields = '';
+let fieldsX = [];
+let fieldsY = [];
+const winFields = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+for (let i = 0; i < 9; i++) {
+  fields += `<div class="field" id = '${i}'></div>`;
+}
+board.innerHTML = fields;
+// END Generate Board
